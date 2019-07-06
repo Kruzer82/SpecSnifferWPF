e20553df6db7396c9f971f88e8f5f535f4516ada
Test <Test@DESKTOP-SHSC4HD>
2019-07-06 22:59:47 +0200

    spec HDD

--- SpecSniffer.Model/SpecReader.cs
+++ SpecSniffer.Model/SpecReader.cs
@@ -13,16 +13,19 @@ namespace SpecSniffer.Model
 
         public SpecReader()
         {
-            SetManufacturerModelSerial();
+            GetManufacturerModelSerial();
 
-
+            CurrentSpec.Processor= GetFromWmi("root\\CIMV2", "Win32_Processor", "Name");
+            GetRam();
+            CurrentSpec.Optical = GetFromWmi("root\\CIMV2", "Win32_CDROMDrive", "MediaType");
+            GetDisk();
         }
 
 
 
 
 
-        private void SetManufacturerModelSerial()
+        private void GetManufacturerModelSerial()
         {
             CurrentSpec.Manufacturer = GetFromWmi("root\\CIMV2", "Win32_ComputerSystem", "Manufacturer");
             string serial= GetFromWmi("root\\CIMV2", "Win32_SystemEnclosure", "SerialNumber");
@@ -42,6 +45,110 @@ namespace SpecSniffer.Model
             }
         }
 
+        private void GetRam()
+        {
+            List<object> ramList = new List<object>();
+            try
+            {
+                //get ram as object list
+                foreach (var queryObj in new ManagementObjectSearcher("root\\CIMV2",
+                   $"SELECT Capacity " +
+                   $"FROM Win32_PhysicalMemory").Get())
+                {
+                    ramList.Add(queryObj["Capacity"]);
+                }
+                //converts to intlist
+                var ramInt = ramList
+                .OfType<ulong>()
+                .Select(s => s / (1024 * 1024 * 1024))
+                .Select(Convert.ToInt32)
+                .ToList();
+
+                //return full RAM and RAM per bank
+                CurrentSpec.Ram= string.Format($"{ramInt.Sum().ToString()}GB ({string.Join("+", ramInt)})");
+            }
+            catch (Exception)
+            {
+                //ignore
+            }
+        }
+
+        /// <summary>
+        /// Method cant check disk type(HDD or SSD) but can better filter external drives.
+        /// (due some of external USB adapters are not displaying as USB but as SCSI)
+        /// </summary>
+        //private void GetDisk()
+        //{
+        //    List<string> hddSize = new List<string>();
+        //    List<string> hddModel = new List<string>();
+        //    List<string> hddSerial = new List<string>();
+        //    //gets size serial and model
+        //    try
+        //    {
+        //        foreach (var query in new ManagementObjectSearcher("root\\CIMV2",
+        //                          $"SELECT Size,Model,MediaType,SerialNumber " +
+        //                          $"FROM Win32_DiskDrive").Get())
+        //        {
+        //            bool isInternal = query["MediaType"].ToString() == "Fixed hard disk media" ? true : 
+        //                              query["MediaType"].ToString() == "Format is unknown" ? true : false;
+
+        //            if (isInternal)
+        //            {
+        //                hddSize.Add(((UInt64)query["Size"] / (1000 * 1000 * 1000)).ToString());
+        //                hddModel.Add((string)query["Model"]);
+        //                hddSerial.Add((string)query["SerialNumber"]);
+        //            }
+        //        }
+
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //    }
+
+        //    CurrentSpec.DiskSize = string.Join("/", hddModel);
+        //    CurrentSpec.DiskName = string.Join("/", hddModel);
+        //    CurrentSpec.DiskName = string.Join("/", hddModel);
+        //}
+
+        
+        
+        private void GetDisk()
+        {
+            List<string> hddSize = new List<string>();
+            List<string> hddModel = new List<string>();
+            List<string> hddSerial = new List<string>();
+            try
+            {
+                foreach (var query in new ManagementObjectSearcher("root\\Microsoft\\Windows\\Storage",
+                                  $"SELECT BusType,Size,MediaType,Model,SerialNumber " +
+                                  $"FROM MSFT_PhysicalDisk").Get())
+                {
+                    bool isNoUSB = (ushort)query["BusType"] == 7 ? false : true;
+
+                    if (isNoUSB)
+                    {
+                        string size = ((UInt64)query["Size"] / (1000 * 1000 * 1000)).ToString();
+                        string type = (ushort)query["MediaType"] == 4 ? "SSD" : (ushort)query["MediaType"] == 3 ? "HDD" : "GB";
+
+                        hddSize.Add($"{size}{type}");
+                        hddModel.Add((string)query["Model"]);
+                        hddSerial.Add((string)query["SerialNumber"]);
+                    }
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            CurrentSpec.DiskSize = string.Join("/", hddSize);
+            CurrentSpec.DiskName = string.Join("/", hddModel);
+            CurrentSpec.DiskSerial = string.Join("/", hddSerial);
+        }
 
         private string GetFromWmi(string scopeNamespace, string scopeClass, string scopeProperty)
         {
--- SpecSniffer.Tests/SpecReaderTests.cs
+++ SpecSniffer.Tests/SpecReaderTests.cs
@@ -21,13 +21,26 @@ namespace SpecSniffer.Tests
             expectedSpec = new Spec() {
                 Manufacturer = "LENOVO",
                 Model = "ThinkPad P50",
-                Serial= "S120en0005mbPC0C3D4N"
+                Serial= "S120en0005mbPC0C3D4N",
+                Processor= "Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz",
+                Ram="16GB (8+8)",
+                Optical="",
+                DiskName= "INTEL SSDPEKKF256G7H",
+                DiskSize="256SSD",
+                DiskSerial= "0000_0000_0100_0000_E4D2_5C1E_9BE4_4E01."
             };
             actualSpec = specReader.CurrentSpec;
 
             Assert.AreEqual(expectedSpec.Manufacturer, actualSpec.Manufacturer, "Manufacturer value not equal.");
             Assert.AreEqual(expectedSpec.Model, actualSpec.Model,"Model value not equal.");
             Assert.AreEqual(expectedSpec.Serial, actualSpec.Serial, "Serial value not equal.");
+            Assert.AreEqual(expectedSpec.Processor, actualSpec.Processor, "Processor value not equal.");
+            Assert.AreEqual(expectedSpec.Ram, actualSpec.Ram, "Ram value not equal.");
+            Assert.AreEqual(expectedSpec.Optical, actualSpec.Optical, "Optical value not equal.");
+
+            Assert.AreEqual(expectedSpec.DiskName, actualSpec.DiskName, "DiskName value not equal.");
+            Assert.AreEqual(expectedSpec.DiskSize, actualSpec.DiskSize, "DiskSize value not equal.");
+            Assert.AreEqual(expectedSpec.DiskSerial, actualSpec.DiskSerial, "DiskSerial value not equal.");
         }
 
 
