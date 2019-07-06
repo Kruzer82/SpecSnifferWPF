f48abf73d21469959eebe2381b8526b941263b7e
Test <Test@DESKTOP-SHSC4HD>
2019-07-07 00:08:12 +0200

    Linked spec to view

---
+++ SpecSniffer.ViewModel/SpecViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SpecSniffer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecSniffer.ViewModel
+{
+    public class SpecViewModel
+    {
+        public Spec ViewModelSpec { get; set; }
+
+
+        public SpecViewModel()
+        {
+            SpecReader specReader = new SpecReader();
+            ViewModelSpec = specReader.CurrentSpec;
+        }
+    }
+}
